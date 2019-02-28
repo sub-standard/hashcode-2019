4070c71aab606da91246bed433a500081ad99b6d
Jordan Mussi <JordanMussi@users.noreply.github.com>
2019-02-28 20:00:11 +0000

    Work on slides instead

--- src/NearestNeighbour.py
+++ src/NearestNeighbour.py
@@ -1,44 +1,42 @@
 from src.Photo import Photo
 import random
-from src.Slide import Slide
 from src.Relationship import get_relationship
 
-def run(photos):
+def run(slides):
     max_diff = 4
     limit = 1000
     # Sort and filter
-    photos = [x for x in photos if x.isHorizontal]
-    photos = sorted(photos, key=lambda x: len(x.tags), reverse=True)
+    slides = sorted(slides, key=lambda x: len(x.tags), reverse=True)
 
-    print("Filtered size: " + str(len(photos)))
+    print("Filtered size: " + str(len(slides)))
 
-    starting_item = photos[0]
+    starting_item = slides[0]
 
-    photos.remove(starting_item)
+    slides.remove(starting_item)
     slideshow = []
-    slideshow.append(Slide(starting_item))
+    slideshow.append(starting_item)
 
     print()
-    while len(photos) > 0:
+    while len(slides) > 0:
         max_score = -1
         best_img = None
-        for i in range(len(photos)):
-            #if abs(len(photos[i].tags) - len(slideshow[-1].tags) > max_diff) or i > limit:
+        for i in range(len(slides)):
+            #if abs(len(slides[i].tags) - len(slideshow[-1].tags) > max_diff) or i > limit:
             if i > limit:
-                #print("current photo len",len(photos[i].tags))
+                #print("current photo len",len(slides[i].tags))
                 #print("last slide len",len(slideshow[-1].tags))
                 #print("exit")
                 break
-            score = get_relationship(slideshow[-1].tags, photos[i].tags)
+            score = get_relationship(slideshow[-1].tags, slides[i].tags)
             if score > max_score:
-                best_img = photos[i]
+                best_img = slides[i]
                 max_score = score
         if best_img is not None:
-            slideshow.append(Slide(best_img))
-            photos.remove(best_img)
+            slideshow.append(best_img)
+            slides.remove(best_img)
         else:
             break
-        print("\rPhotos left: "+str(len(photos)) + "          ", end="")
+        print("\rSlides left: "+str(len(slides)) + "          ", end="")
     print("\nDone")
 
     return slideshow
