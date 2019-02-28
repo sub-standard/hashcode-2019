1c59306a0b133fdc90c551f01c41624a4b1d8488
BrandonAGoodwin <35398295+BrandonAGoodwin@users.noreply.github.com>
2019-02-28 18:19:13 +0000

    Adjusted the Slide class

--- src/Slide.py
+++ src/Slide.py
@@ -3,11 +3,6 @@ class Slide():
     def __init__(self, photoOne, photoTwo = None):
         self.photoOne = photoOne
         self.photo = photoTwo
-
-    def getTags(self):
-        tags = self.photoOne.tags
-        if(self.photoTwo is not None):
-            tags = tags.union(self.photoTwo.tags)
-        return tags
-
-
+        self.tags = self.photoOne.tags
+        if (self.photoTwo is not None):
+            self.tags = self.tags.union(self.photoTwo.tags)
